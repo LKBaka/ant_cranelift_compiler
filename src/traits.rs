0a0b4aaa2cc617bf2a09e8eda972dfa4083348ba
APIBox <LinkerVB@outlook.com>
2025-12-28 09:27:16 +0800

    feat(traits): 添加 Trait NeedGc

--- src/traits.rs
+++ src/traits.rs
@@ -1,3 +1,5 @@
+use ant_type_checker::ty::Ty;
+
 pub trait NoRepeatPush<T> {
     fn push_no_repeat(&mut self, item: T);
 }
@@ -6,6 +8,26 @@ impl<T: Eq> NoRepeatPush<T> for Vec<T> {
     fn push_no_repeat(&mut self, item: T) {
         if !self.contains(&item) {
             self.push(item);
+        }
+    }
+}
+
+pub trait NeedGc {
+    fn need_gc(&self) -> bool;
+}
+
+impl NeedGc for Ty {
+    fn need_gc(&self) -> bool {
+        match self {
+            Ty::BigInt => true,
+            Ty::Function { .. } => false,
+            Ty::Struct { .. } => true,
+            Ty::Generic(_) => true,
+            Ty::IntTy(_) => false,
+            Ty::Bool => false,
+            Ty::Unit => false,
+            Ty::Str => false,
+            Ty::Unknown => false,
         }
     }
 }
